726c0131727da39d14817a043ef079b61c9650fc
Alexandro <56030623+josealexandro@users.noreply.github.com>
2022-04-29 15:54:48 -0300

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route} from 'react-router-dom';
+
+import Login from '../pages/Login';
+
+const Routes: React.FC = () =>  (
+    <Switch>
+        <Route path="/" component={Login}  />
+    </Switch>
+)
+
+export default Routes;
+
+
+
+
